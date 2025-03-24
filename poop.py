9057f24a9ba8ada489a18b985d1e5effa055f877
avac886 <avac886@gmail.com>
2025-03-24 09:11:24 -0400

    wow

---
+++ poop.py
@@ -0,0 +1,6 @@
+# 6. Final Challenge
+# Use map() to convert Fahrenheit to Celsius, skipping the first item
+temperatures = ["Label", 32, 50, 77, 204]
+celsius = []
+for data in temperatures[1:]:
+    celsius = map[lambda x: (x-32)*5/9, temperatures]
